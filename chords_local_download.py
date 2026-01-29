e3ccd1533a469e297e1589c74c693acf942de4f2
Rebecca Zieber <rzieber@ucar.edu>
2026-01-29 15:14:34 +0200

    Update chords_local_download.py

--- chords_local_download.py
+++ chords_local_download.py
@@ -121,7 +121,7 @@ def main():
     ]
     if portal_name.lower() not in portal_lookup:
         raise ValueError(f"Please enter one of the following portal names (case insensitive):\n\t \
-                            Barbados, Trinidad, 3D-PAWS, Calibration, FEWSNET, Kenya, Zimbabwe, Zambia, Argentina, IITM, Dominican-Republic, Fiji,"\
+                            Barbados, Trinidad, 3D-PAWS, Calibration, FEWSNET, Kenya, Zimbabwe, Zambia, Argentina, IITM, Dominican-Republic, Fiji, "\
                             "Malawi, Bahamas, Somalia")
     
     # processing loop ------------------------------------------------------------------------------------------------------------------
