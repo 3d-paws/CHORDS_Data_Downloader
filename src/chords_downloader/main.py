7252dca433962df94af29ea69a295a4037aa68ef
rebecca-zieber <rzieber@ucar.edu>
2026-02-18 15:58:41 -0700

    Support for IITM and Zambia

--- src/chords_downloader/main.py
+++ src/chords_downloader/main.py
@@ -6,16 +6,14 @@ Authored by Rebecca Zieber
 from pathlib import Path
 from chords_downloader import chords_local_download, chords_dataframes
 
-PORTAL_URL = r"https://chords.url.com/"
-PORTAL_NAME = "Portal Name"
-DATA_PATH = Path("C://path//to//local//storage//") 
-INSTRUMENT_IDS = [
-    1,2,3
-]
-USER_EMAIL = 'your@email.com'
-API_KEY = 'your-api-key' 
-START = 'YYYY-MM-DD HH:MM:SS' 
-END = 'YYYY-MM-DD HH:MM:SS'
+PORTAL_URL = r"https://3d-kenya.chordsrt.com/"
+PORTAL_NAME = "Kenya"
+DATA_PATH = Path("/Users/rzieber/Downloads") 
+INSTRUMENT_IDS = list(range(45))
+USER_EMAIL = 'rzieber@ucar.edu'
+API_KEY = 'GbwLdKSxv8yL4Jqk4T7b' 
+START = '2024-02-18 00:00:00' 
+END = '2026-02-18 23:59:59'
 
 def main():
     # To download csv's locally to your machine (most common):
